e303dfec871882bd484d0ac884daf7a4853ca053
lucas144hz <104213322+lucas144hz@users.noreply.github.com>
2022-07-19 19:56:59 -0300

    Update add_venda.php

--- add_venda.php
+++ add_venda.php
@@ -1,7 +1,20 @@
 <?php
-
+include("conect.php");
 $dados = filter_input_array(INPUT_POST, FILTER_SANITIZE_SPECIAL_CHARS); 
-var_dump($dados);
+$numero_do_boi = $_POST['numero_do_boi'];
+
+$peso_do_boi = $_POST['peso_do_boi'];
+
+$data_da_pesagem = $_POST['data_da_pesagem'];
+
+$data_de_venda = $_POST['data_de_venda'];
+
+$query = "INSERT INTO public.venda(
+	numero_do_boi, peso_do_boi, data_da_pesagem, data_de_venda)
+	VALUES ('$numero_do_boi', '$peso_do_boi', '$data_da_pesagem', '$data_de_venda')";
+
+$estado = $conexao->query($query);
+header('Location:index.html');
 
 
 ?>
